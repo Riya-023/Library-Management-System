a109e0562e77389542a867794181c89d75dd3240
Riya <riyabhatia230@gmail.com>
2025-08-04 12:43:47 +0530

    Updated bookController

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -5,14 +5,12 @@ import { User } from "../models/UserModels.js";
 import ErrorHandler from "../middlewares/errorMiddleware.js";
 
 export const addBook = catchAsyncErrors(async(req, res, next)=>{
-    const {title, author, description, price, quantity} = req.body; // ye chize chahiye rahegi
+    const {title, author, description, price, quantity} = req.body; 
 
-    //agar hum inme se koi bhi ek chiz nhi dalte h toh if() wale se error ayega
     if (!title || !author || !description || !price || !quantity) {
         return next(new ErrorHandler("Please fill all fields.", 400));
     }
 
-    //ye book create krne ke liye
     const book = await Book.create({
         title,
         author,
@@ -21,7 +19,6 @@ export const addBook = catchAsyncErrors(async(req, res, next)=>{
         quantity,
     });
 
-    // add hone ke baad 201(jo create krne ka status code h vo ayega) or messge jo h print hoga
     res.status(201).json({
         success: true,
         message: "Book added sucessfully.",
